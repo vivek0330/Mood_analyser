d5fa02276046a4e7be342354feebdbe740c043b0
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-31 00:51:22 +0530

    Add: refactor

--- MoodAnalyser/src/main/java/com/bridgelabz/MoodAnalyser.java
+++ MoodAnalyser/src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -15,19 +15,13 @@ public class MoodAnalyser {
 
     /**
      * Purpose : Given a message, ability to analyse and respond HAPPY or SAD mood.
-     * Condition 1 : Handle NULLPOINTER Exception using try-catch block.
-     * Condition 2 : Mood analysis exception for null or empty message.
+     *
+     * @return : HAPPY or SAD
      */
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-            if (message.length() == 0)
-                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, "Please enter proper message!");
-            if (message.contains("sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        } catch (NullPointerException e) {
-            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL, "Please enter proper message!");
-        }
+    public String analyseMood() {
+        if (message.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
--- MoodAnalyser/src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ MoodAnalyser/src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -4,76 +4,32 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
-import org.junit.Test;
-
 public class MoodAnalyserTest {
 
     /**
      * Purpose : analyse mood
      * Input message : This is a sad message.
+     *
      * @return : SAD
      */
     @Test
     public void testMoodAnalysis_whenMoodIsSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("This is a sad message");
-        String mood = null;
-        try {
-            mood = moodAnalyser.analyseMood();
-            Assert.assertEquals("SAD" , mood);
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertThat(mood, CoreMatchers.is("SAD"));
     }
 
     /**
      * Purpose : analyse mood
      * Input message: Iam in any mood.
+     *
      * @return : HAPPY
      */
     @Test
     public void testMoodAnalysis_whenMoodIsHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("Iam in any mood");
-        String mood = null;
-        try {
-            mood = moodAnalyser.analyseMood();
-            Assert.assertEquals("HAPPY" , mood);
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Purpose : Custom Exception if User Provides Invalid Mood
-     * Input message: null
-     * @return : custom exception message is null
-     */
-
-    @Test
-    public void testMoodAnalysis_whenMoodIsNull_ShouldThrowException() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        try {
-            moodAnalyser.analyseMood();
-        } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.type);
-        }
-    }
-
-    /**
-     * Purpose : Custom Exception if User Provides Invalid Mood
-     * Input message: empty
-     * @return : custom exception message is empty
-     */
-
-    @Test
-    public void testMoodAnalysis_whenMoodIsEmpty_ShouldThrowException() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("");
-        try {
-            moodAnalyser.analyseMood();
-        } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , e.type);
-        }
+        MoodAnalyser moodAnalyser = new MoodAnalyser("Im in any mood");
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
     }
 
 }
