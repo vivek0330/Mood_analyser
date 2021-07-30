451f1155626027af466827e99a8ca31f57b0513f
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-31 00:42:35 +0530

    Add: null pointer exception

--- MoodAnalyser/src/main/java/com/bridgelabz/MoodAnalyser.java
+++ MoodAnalyser/src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,16 +2,32 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
 
+    private String message;
+
+    //Default Constructor
+    public MoodAnalyser() {
+    }
+
+    //Parameterized Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
     /**
-     * Purpose : Given a message, ability to analyse and respond HAPPY or SAD mood
-     *
-     * @param : message
+     * Purpose : Given a message, ability to analyse and respond HAPPY or SAD mood.
+     * Condition: Handle nullpointerException using try-catch block.
+     *  @param : message
+     * @param this_is_a_sad_message
      * @return : HAPPY or SAD
      */
-    public String analyseMood(String message) {
-        if (message.contains("sad"))
-            return "SAD";
-        else
+    public String analyseMood(String this_is_a_sad_message) {
+        try {
+            if (message.contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (Exception e) {
             return "HAPPY";
+        }
     }
 }
--- MoodAnalyser/src/test/java/com/bridgelabz/MoodTest.java
+++ MoodAnalyser/src/test/java/com/bridgelabz/MoodTest.java
@@ -5,29 +5,42 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodTest {
+
     /**
      * Purpose : analyse mood
      * Input message : This is a sad message.
-     *
      * @return : SAD
      */
     @Test
     public void testMoodAnalysis_whenMoodIsSad() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser();
-        String mood = moodAnalyzer.analyseMood("This is a sad message");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is a sad message");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
     }
 
     /**
      * Purpose : analyse mood
-     * Input message: Im in any mood.
-     *
+     * Input message: Iam in any mood.
      * @return : HAPPY
      */
     @Test
     public void testMoodAnalysis_whenMoodIsHappy() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser();
-        String mood = moodAnalyzer.analyseMood("Im in any mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
     }
+
+    /**
+     * Purpose : Handle Exception if User Provides Invalid Mood
+     * Input message: null
+     * @return : HAPPY
+     */
+
+    @Test
+    public void testMoodAnalysis_whenMoodIsNull() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY" , mood);
+    }
+
 }
